264a87dfdfca127434831b44681124790159d009
Roman <sprigg4n.me@gmail.com>
2024-03-07 19:15:59 +0100

    first init

---
+++ src/test/java/com/example/cross_prog_2/CrossProg2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cross_prog_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrossProg2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
